1a3768a4610c5c280dd73d81e937f5a0f25153d9
dangwin <danghwin@gmail.com>
2021-05-22 09:51:02 -0500

    commit tree 65ea583d79c1d38db0a98b8112cc6ecedb648629

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 // create your App component here
+import { Component } from "react";
+
+class App extends Component {
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+    }
+    render(){
+        return(
+            null
+        )
+    }
+}
+
+export default App
